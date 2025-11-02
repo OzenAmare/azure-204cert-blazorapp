f11cd6579260f7a9c92e569f231038834dbce5ae
Nick Law <89273070+OzenAmare@users.noreply.github.com>
2025-11-02 13:00:38 -0500

    safely added cosmos db client

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using BlazorApp.Components;
 using Serilog;
+using Azure.Identity;
+using Azure.Core;
+using Microsoft.Azure.Cosmos; 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+var key = builder.Configuration["Cosmos:Key"];
 
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
@@ -17,7 +21,22 @@ builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddRazorComponents() 
     .AddInteractiveServerComponents();
-    
+
+builder.Services.AddSingleton<TokenCredential>(sp =>
+{
+
+    return new DefaultAzureCredential();
+});
+
+builder.Services.AddSingleton(sp =>
+{
+    var endpoint = "https://blazorexperimentcosmos.documents.azure.com:443/";
+
+    var key = "key";
+
+    return new CosmosClient(endpoint, key);
+
+});
 var app = builder.Build();   
 
 
